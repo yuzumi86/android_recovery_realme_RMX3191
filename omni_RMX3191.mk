681840217e4fa7b84951fcea5a1d4eeb8c340c7b
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-08-27 08:57:29 +0700

    RMX3191: Rewrite to RMX3191

--- omni_RMX3191.mk
+++ omni_RMX3191.mk
@@ -25,13 +25,13 @@ $(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
-PRODUCT_DEVICE := RMX2020
-PRODUCT_NAME := omni_RMX2020
+PRODUCT_DEVICE := RMX3191
+PRODUCT_NAME := omni_RMX3191
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := RMX2020
+PRODUCT_MODEL := RMX3191
 PRODUCT_MANUFACTURER := realme
-TW_DEVICE_VERSION := For RealmeUI 2.0 Base
-PRODUCT_SHIPPING_API_LEVEL := 29
+TW_DEVICE_VERSION := For Realme 2.0
+PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
